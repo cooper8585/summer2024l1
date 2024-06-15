6064916f95c42675b03150be985eb747e4cdaac2
cooper8585 <pawel.bednarz85@gmail.com>
2024-06-15 10:17:14 +0000

    third commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include "mbed.h"
+#include <cstdio>
+/*
+a few words max 100 on the program
+*/
+InterruptIn bluebtn(PC_13);
+volatile int int_trig = 0;
+
+void bttnFall(){
+    int_trig = 1;
+}
+
 
 // main() runs in its own thread in the OS
 int main()
 {
+    bluebtn.fall(&bttnFall);
+
     // this is lab 1 embedded sys module
     while (true) {
+        if(int_trig ==1){
+            int_trig = 0;
+            printif("bttnFall \r\n");
+            
+        }
     }
 }
 
